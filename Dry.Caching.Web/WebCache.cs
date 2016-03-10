7e9918a66f15e3b0920caab1e587c1d5785746b3
Gary Hall <gary.hall@fourminutemile.co.uk>
2016-03-10 16:09:45 +0000

    Added web cache implementation

---
+++ Dry.Caching.Web/WebCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Caching;
+
+namespace Dry.Caching.Web
+{
+    public class WebCache : ICacheWithExpirationCallback
+    {
+        private readonly Cache cache;
+
+        public WebCache(Cache cache)
+        {
+            if (cache == null)
+                throw new ArgumentNullException(nameof(cache));
+
+            this.cache = cache;
+        }
+
+        public T Get<T>(string key)
+        {
+            return (T)cache.Get(key);
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            cache.Insert(key, value);
+        }
+
+        public void Set<TValue>(string key, TValue value, TimeSpan slidingExpiration)
+        {
+            cache.Insert(key, value, null, Cache.NoAbsoluteExpiration, slidingExpiration);
+        }
+
+        public void Set<TValue>(string key, TValue value, DateTime absoluteExpiration)
+        {
+            cache.Insert(key, value, null, absoluteExpiration, Cache.NoSlidingExpiration);
+        }
+
+        public void Set<TValue>(string key, TValue value, TimeSpan slidingExpiration, Func<TValue> onExpiry)
+        {
+            cache.Insert(key, value, null, Cache.NoAbsoluteExpiration, slidingExpiration);
+        }
+
+        public void Set<TValue>(string key, TValue value, DateTime absoluteExpiration, Func<TValue> onExpiry)
+        {
+            cache.Insert(key, value, null, absoluteExpiration, Cache.NoSlidingExpiration);
+        }
+    }
+}
